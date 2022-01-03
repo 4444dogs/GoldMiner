d57f2a1d47ec25ce67673c574972381c60167faa
4444dogs <lee@juszak.net>
2022-01-02 17:47:55 -0700

    1.0.2

--- goldminer.py
+++ goldminer.py
@@ -1,16 +1,26 @@
+##Installing Tkinter
 from tkinter import *
 
+##What Happens When You Mine Gold
 def mined():
     lbl.configure(text="You have mined gold!")
 
+
+##Window Settings
 window = Tk()
 window.title("Super Cool Random App")
 window.state('zoomed')
 
+##The 'Mine Gold' Button
 btn = Button(window, text="Mine Gold", command=mined)
 btn.grid(column=0, row=0)
 
+##The Text
 lbl = Label(window, text="You have not mined gold.")
 lbl.grid(column=1, row=0)
 
+##The Window
 window.mainloop()
+
+##NOTES
+##This app was made as a joke but I continue to update anyways. Anyone can use the code above.
